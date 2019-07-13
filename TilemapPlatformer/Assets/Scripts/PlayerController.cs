3283aa6be61fad8235c5fbf9c0fe0c21849fcf12
BrotherMutton <52295709+BrotherMutton@users.noreply.github.com>
2019-07-13 16:07:01 -0400

    Project complete

--- TilemapPlatformer/Assets/Scripts/PlayerController.cs
+++ TilemapPlatformer/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    private Rigidbody2D rb2d;
+
+    public float speed;
+    public float jumpforce;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,4 +21,32 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0,jumpforce), ForceMode2D.Impulse);
+            }
+        }
+    }
+
 }
